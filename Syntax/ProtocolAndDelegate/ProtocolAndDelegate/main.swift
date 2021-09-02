9155ee30f53b206d547eb6de27c1ce17d170ebe8
Jaeyoung Lee <ejei0g@gmail.com>
2021-09-02 11:31:36 +0900

    Delegate pattern

---
+++ Syntax/ProtocolAndDelegate/ProtocolAndDelegate/main.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+protocol AdvancedLifeSupport {
+    func perfomeCPR()
+}
+
+class EmergencyCallHandler {
+    var delegate: AdvancedLifeSupport?
+    
+    func chk() {
+        print("iamcalling...")
+    }
+    func medicalEmergency() {
+        if let bleep = delegate {
+            bleep.perfomeCPR()
+        }
+    }
+   
+}
+
+class Doctor: AdvancedLifeSupport {
+    init(handler: EmergencyCallHandler) {
+        handler.delegate = self
+    }
+    func perfomeCPR() {
+        print("Doctor perform")
+    }
+    func test() {
+        print("test")
+    }
+}
+let a = EmergencyCallHandler()
+let doctor = Doctor(handler: a)
+
+a.chk()
+a.medicalEmergency() //trigger 수행
+a.delegate?.perfomeCPR()
